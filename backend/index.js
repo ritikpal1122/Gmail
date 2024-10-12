2b99c9ac3737163089ff4b54a26bea77c69b72bd
DebugDynamo <er.ritikpal@gmail.com>
2024-10-13 02:38:54 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRoute from "./routes/user.route.js";
 import emailRoute from "./routes/email.route.js";
+import path from 'path'
 
 dotenv.config({});
 connectDB();
 const PORT = 8080;
 const app = express();
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.urlencoded({extended:true}));
@@ -22,9 +25,16 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
+
+
 // routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/email", emailRoute);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get("*" , (req,res) => {
+    res.sendFile(path.resolve(_dirname,"frontend" , "dist" , "index.html"));
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server running at port ${PORT}`);
